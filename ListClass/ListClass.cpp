e1f5308027bf14c9536dfb2e7b97d8c18fb33603
Hayk Hovhannisyan <hovahnnisayn@mail.ru>
2023-09-26 15:38:06 +0400

    adding smth

--- ListClass/ListClass.cpp
+++ ListClass/ListClass.cpp
@@ -6,25 +6,45 @@ private:
 
 	struct Node {
 		Node* next;
-		K element;
-		Node(T& x) :element(x), next(nullptr) {}
+		T element;
+		Node(const T& x) :element(x), next(nullptr) {}
+		~Node() {
+			delete next;
+		}
 	};
 	Node* head;
 public:
 	List() :head(nullptr) {}
-
+	~List() {
+		delete head;
+	}
 	void push_back(const T& x) {
 		Node* currentnode = head;
-		whlie(currentnode->next != nullptr) {
+		if (currentnode == nullptr) {
+			head = new Node(x);
+			return;
+		}
+		while (currentnode->next != nullptr) {
 			currentnode = currentnode->next;
 		}
 		currentnode->next = new Node(x);
+	}
+	void printList() {
+		Node* currentnode = head;
+		while (currentnode != nullptr) {
+			std::cout << currentnode->element << " ";
+			currentnode = currentnode->next;
+		}
+		std::cout << std::endl;
 	}
 
 };
 int main()
 {
-
+	List <int> lst;
+	lst.push_back(1);
+	lst.push_back(2);
+	lst.printList();
 }
 
 
