1c4abf6daf8d62e667eb6ed60f924484a3889e18
Hayk Hovhannisyan <hovahnnisayn@mail.ru>
2023-10-01 18:44:49 +0400

    Almost done task 2

--- Class_Vector_with_template.cpp
+++ Class_Vector_with_template.cpp
@@ -98,25 +98,24 @@ public:
 			capacity_ *= 2;
 			T* tmp = new T[capacity_];
 			for (size_t i = 0; i < size_; ++i) {
-				tmp[i] = std::move(vec[i]);
+				tmp[i] = vec[i];
 			}
-			delete[]vec;
+			delete[] vec;
 			vec = tmp;
-			tmp = nullptr;
 		}
 		vec[size_] = std::move(x);
 		++size_;
 	}
+
 	void push_back(T&& x) {
 		if (size_ == capacity_) {
 			capacity_ *= 2;
 			T* tmp = new T[capacity_];
 			for (size_t i = 0; i < size_; ++i) {
-				tmp[i] = std::move(vec[i]);
+				tmp[i] = vec[i];
 			}
-			delete[]vec;
+			delete[] vec;
 			vec = tmp;
-			tmp = nullptr;
 		}
 		vec[size_] = x;
 		++size_;
@@ -158,8 +157,85 @@ Vector<int> mul(const Vector<int>& nums) {
 	return result;
 }
 
+void luckynums(Vector<Vector<int>> vec) {
+
+	//2 1 7
+	Vector<int> min_index;
+	Vector<int> max_index;
+	Vector<int> res;
+	std::cout << vec[0][2];
+	//min = 2 min = 1 
+	/*
+	int min;
+	int max;
+	int k;//min index
+	bool ismax = true;
+	if (vec.empty()) {
+		throw std::invalid_argument("The Vector is empty!!!");
+	}
+	for (int i = 0; i < vec.capacity(); ++i) {
+		min = vec[i][0];
+		k = 0;
+		for (int j = 1; j < vec[i].size(); ++j) {
+			if (vec[i][j] < min) {
+				k = j;
+				min = vec[i][j];
+			}
+			else {
+				continue;
+			}
+		}
+		min_index.push_back(k);
+	}
+	for (int j = 0; j < vec.capacity(); ++j) {
+		max = vec[0][j];
+		k = 0;
+		for (int i = 1; i <= vec.capacity() - 1; ++i) {
+			if (vec[i][j] > max) {
+				max = vec[i][j];
+				k = i;
+			}
+		}
+		max_index.push_back(k);
+	}
+	for (int i = 0; i < min_index.size(); ++i) {
+		if (min_index[i] == max_index[i]) {
+			res.push_back(vec[min_index[i]][max_index[i]]);
+		}
+	}
+
+
+		// min_index = 0 0 0   1 1 1
+		// max_index = 2 2 2   2 1 2
+
+		//1 4 7      8 2 5
+		//8 9 10	 9 1 2
+		//14 15 16   10 0 56
+
+
+
+
+	return res;
+	*/
+}
+
 int main()
 {
+	Vector<Vector<int>> a(3);
+	a[0].push_back(8);   //1 4 7
+	a[0].push_back(2);	// 8 9 10
+	a[0].push_back(5);	//14 15 16
+	a[1].push_back(9);	//
+	a[1].push_back(1);
+	a[1].push_back(2);
+	a[2].push_back(10);
+	a[2].push_back(0);
+	a[2].push_back(56);
+	std::cout << a[2][2] << " ";
+	//   luckynums(a);
+	Vector<Vector<int>> b;
+	b = a;
+	std::cout << b[2][2];
 
 
 
